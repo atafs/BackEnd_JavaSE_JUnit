67355ddc0b2f3c1902a338cac7e74e5666ab291e
atafs <atafs@iscte-iul.pt>
2016-01-12 11:28:54 +0000

    adding comments

--- project_001_JUnit_start/src/type/Square.java
+++ project_001_JUnit_start/src/type/Square.java
@@ -2,12 +2,12 @@ package type;
 
 public class Square {
 
-	/** */
+	/** Function: receives an int and returns the product of itself */
 	public int square(int x) {
 		return x*x;
 	}
 	
-	/** */
+	/** Function: counts the number of a char in a string */
 	public int countA(String word) {
 		int count = 0;
 		for (int i = 0; i < word.length(); i++) {
